6cff7491f0106b3473132a8998fab2c74cc691fb
Taufik Nurrohman <hi@taufik-nurrohman.com>
2021-09-05 11:45:56 +0700

    IGNITE!

---
+++ README.md
@@ -0,0 +1,9 @@
+Lot Folder Manager for Mecha&rsquo;s [Panel](https://github.com/mecha-cms/x.panel)
+==================================================================================
+
+Release Notes
+-------------
+
+### 1.0.0
+
+ - [ ] Initial stable release.
---
+++ panel.lot.tasks/index/panel.php
@@ -0,0 +1,14 @@
+<?php
+
+$_['lot']['bar']['lot'][0]['lot']['folder']['lot']['s'] = [
+    'title' => 'Folder',
+    'description' => ['New %s', 'Folder'],
+    'icon' => 'M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z',
+    'url' => $_['/'] . '/::s::/.lot',
+    'stack' => 0
+];
+
+$_['lot']['bar']['lot'][0]['lot']['folder']['lot']['s-separator'] = [
+    'type' => 'separator',
+    'stack' => .1
+];
