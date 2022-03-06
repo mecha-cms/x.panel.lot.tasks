22dcec82953fbed025002ee19b2acc169d7b2c40
Taufik Nurrohman <hi@taufik-nurrohman.com>
2022-03-06 21:55:25 +0700

    Update

---
+++ panel.tweak/tweak/always-hide-test-menu.php
@@ -0,0 +1,6 @@
+<?php
+
+Hook::set('_', function($_) {
+    $_['lot']['bar']['lot'][1]['lot']['test']['skip'] = true;
+    return $_;
+}, 50);
--- panel.tweak/tweak/enable-zip-utility.php
+++ panel.tweak/tweak/enable-zip-utility.php
@@ -6,9 +6,10 @@ Hook::set('_', function($_) use($zip) {
     if (0 === strpos($_['type'] . '/', 'files/')) {
         if (!empty($_['lot']['desk']['lot']['form']['lot'][0]['lot']['tasks'])) {
             $_['lot']['desk']['lot']['form']['lot'][0]['lot']['tasks']['lot']['pack'] = [
-                'active' => $not_void = $zip && !!q(g($folder = $_['folder'] ?? P, 1)),
-                'description' => $not_void ? 'Download as ZIP' : 'Folder is empty',
+                'active' => $not_void = $zip && !!q(g($folder = $_['folder'] ?? P, 1, true)),
+                'description' => 'Download as ZIP',
                 'icon' => 'M5,20H19V18H5M19,9H15V3H9V9H5L12,16L19,9Z',
+                'skip' => !$not_void,
                 'stack' => 11,
                 'title' => false,
                 'type' => 'link',
@@ -48,9 +49,10 @@ Hook::set('_', function($_) use($zip) {
                     ];
                 } else if (is_dir($folder = $v['path'] ?? $k)) {
                     $v['tasks']['pack'] = [
-                        'active' => $not_void = $zip && !!q(g($folder, 1)),
-                        'description' => $not_void ? 'Pack' : 'Folder is empty',
+                        'active' => $not_void = $zip && !!q(g($folder, 1, true)),
+                        'description' => $not_void ? 'Download as ZIP' : 'Folder is empty',
                         'icon' => 'M20 21H4V10H6V19H18V10H20V21M3 3H21V9H3V3M5 5V7H19V5M10.5 11V14H8L12 18L16 14H13.5V11',
+                        'skip' => !empty($v['current']),
                         'stack' => 9,
                         'title' => 'Pack',
                         'url' => $not_void ? [
