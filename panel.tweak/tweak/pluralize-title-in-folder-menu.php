6defd665aaf7e54cbcc7330fa5c86b79b61e6790
Taufik Nurrohman <hi@taufik-nurrohman.com>
2022-03-10 22:12:50 +0700

    Update

--- panel.tweak/tweak/pluralize-title-in-folder-menu.php
+++ panel.tweak/tweak/pluralize-title-in-folder-menu.php
@@ -5,7 +5,13 @@ Hook::set('_', function($_) {
     if (!empty($_['lot']['bar']['lot'][0]['lot']['folder']['lot'])) {
         foreach ($_['lot']['bar']['lot'][0]['lot']['folder']['lot'] as &$v) {
             if (isset($v['title']) && is_string($title = $v['title'])) {
-                if ('s' === substr($title, -1)) {
+                if (
+                    'ch' === substr($title, -2) ||
+                    's' === substr($title, -1) ||
+                    'sh' === substr($title, -2) ||
+                    'x' === substr($title, -1) ||
+                    'z' === substr($title, -1)
+                ) {
                     $v['title'] .= 'es';
                 } else {
                     $v['title'] .= 's';
