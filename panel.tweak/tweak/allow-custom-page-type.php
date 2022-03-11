e7413a6c631ef78f46d6b35f501a03d73bdd77d0
Taufik Nurrohman <hi@taufik-nurrohman.com>
2022-03-11 21:34:04 +0700

    Update

---
+++ panel.tweak/tweak/allow-custom-page-type.php
@@ -0,0 +1,8 @@
+<?php
+
+Hook::set('_', function($_) {
+    if (0 === strpos($_['type'] . '/', 'page/') && isset($_['lot']['desk']['lot']['form']['lot'][1]['lot']['tabs']['lot']['page']['lot']['fields']['lot']['type'])) {
+        $_['lot']['desk']['lot']['form']['lot'][1]['lot']['tabs']['lot']['page']['lot']['fields']['lot']['type']['type'] = 'text';
+    }
+    return $_;
+}, 10.2);
