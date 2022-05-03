df6803d324bd49786ea1370f1a42db96b91d8d5f
Taufik Nurrohman <hi@taufik-nurrohman.com>
2022-05-04 06:15:40 +0700

    Move to Root

--- tweak/show-file-size-after-file-name.php
+++ tweak/show-file-size-after-file-name.php
@@ -4,8 +4,8 @@ Hook::set('_', function($_) {
     if (0 === strpos($_['type'] . '/', 'files/')) {
         if (!empty($_['lot']['desk']['lot']['form']['lot'][1]['lot']['tabs']['lot']['files']['lot']['files']['lot'])) {
             foreach ($_['lot']['desk']['lot']['form']['lot'][1]['lot']['tabs']['lot']['files']['lot']['files']['lot'] as $k => &$v) {
-                if (is_file($file = $v['path'] ?? $k) && !array_key_exists('info', $v)) {
-                    $v['info'] = '(' . size(filesize($file)) . ')';
+                if (is_file($file = $v['path'] ?? $k) && !array_key_exists('status', $v)) {
+                    $v['status'] = size(filesize($file));
                 } else if (isset($v['type']) && 'file' === $v['type']) {
                     // Fake!
                 }
